612914dd266e8e977e36862a31c62649972ae10d
Soban <114462166+SobanAnjum07@users.noreply.github.com>
2023-01-31 23:37:26 +0500

    Update 469A_i_wanna_be_the_guy.py

--- 469A_i_wanna_be_the_guy.py
+++ 469A_i_wanna_be_the_guy.py
@@ -1,13 +1,10 @@
-def i_became_the_guy(n, l1, l2):
-    l = l1 + l2
-    s = set(l)
-    if len(s) >= n and (n in s):
-        return 'I become the guy.'
-    else:
-        return 'Oh, my keyboard!'
-        
 if __name__ == '__main__':
     n = int(input())
     l1 = list(map(int, input().split()))
     l2 = list(map(int, input().split()))
-    print(i_became_the_guy(n, l1, l2))
+    l = l1[1:] + l2[1:]
+    s = set(l)
+    if len(s) >= n and (n in s):
+        print('I become the guy.')
+    else:
+        print('Oh, my keyboard!')
