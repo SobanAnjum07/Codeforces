469110cbf361ea9f1c517f8259d3d0e7e8d8d950
Soban <114462166+SobanAnjum07@users.noreply.github.com>
2023-03-12 11:38:26 +0500

    Create 1754A _technical_support.py

---
+++ 1754A _technical_support.py
@@ -0,0 +1,12 @@
+t = int(input())
+for i in range(t):
+    n = int(input())
+    s = input()
+    count = 0
+    for ch in s:
+        if ch == 'Q':
+            count += 1
+        elif count > 0:
+            count -= 1
+    if count <= 0: print('Yes')
+    else: print('No')
