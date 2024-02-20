358a395e34615ad016982415811048b3705954bf
Soban <msobananjum786@gmail.com>
2024-02-20 13:59:48 +0500

    312A - Even Odds

---
+++ 312A - Even Odds.py
@@ -0,0 +1,22 @@
+
+
+def sol(n, index):
+
+    if n%2==0:
+        if index <= n//2:
+            return (index*2)-1
+        else:
+            return 2 *(index -n//2)
+    else:
+        if index <=(n//2+1):
+            return 2 *(index-1)+1
+        else:
+            return 2 *(index -((n//2)+1))
+        
+
+
+
+if __name__ == "__main__":
+    
+    n, index = map(int, input().split())
+    print(sol(n ,index))
