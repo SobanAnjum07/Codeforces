48520937bba451f4e4ad58d502b90314bbb85254
Soban <msobananjum786@gmail.com>
2024-03-02 20:11:36 +0500

    Updated

--- team.py
+++
@@ -1,9 +0,0 @@
-def main():
-    n = int(input())
-    count = 0
-    for i in range(n):
-        x = list(map(int, input().split()))
-        if sum(x) >= 2:
-            count += 1
-    print(count)
-main()
