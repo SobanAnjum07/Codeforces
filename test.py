c4f33cbf86ac4dda26de4ba6a3d1e2c39a7f81b5
Soban <msobananjum786@gmail.com>
2024-05-02 23:31:46 +0500

    CODE

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ input = sys.stdin.readline
 #! rggry
 # ? [0, 3] [1, 2]
 
+
 def sol(s:str, n:int, c:str):
     index_ele  = [index for index, char in enumerate(s) if char == c]
     index_green = [index for index, char in enumerate(s) if char == "g"]
@@ -23,5 +24,7 @@ if __name__ == "__main__":
         s = input()
         
         print(sol(s, int(n), c))
+    
+    
     
     
