464365e8a440c9fa1e2af2b631783e836b0df518
Soban <114462166+SobanAnjum07@users.noreply.github.com>
2024-05-07 14:28:01 +0500

    CODE

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
+
+
 import sys
+from collections import defaultdict
 input = sys.stdin.readline
+
+
 
 # 5 r
 #! rggry
 # ? [0, 3] [1, 2]
 
 
-def sol(s:str, n:int, c:str):
+"""def sol(s:str, n:int, c:str):
     index_ele  = [index for index, char in enumerate(s) if char == c]
     index_green = [index for index, char in enumerate(s) if char == "g"]
     
@@ -14,16 +19,30 @@ def sol(s:str, n:int, c:str):
         return index_green[-1] - index_ele[0] + 1
     else:
         return (index_green[-1] + 1) - (index_ele[0]+1)
+"""
+def sol1(s,n):
+    i=0; j=1
+    hash_map = defaultdict(int)
     
+    while j < len(s):
+        ap = s[:i] + s[j+1:]
+        
+        if hash_map[ap] == 0:
+            hash_map = 1
+        
+        i+=1; j+=1
+        
+    val = hash_map.values()
+    return sum(val)
 
     
 if __name__ == "__main__":
     
     for _ in range(int(input())):
-        n ,c = map(str, input().split())
+        n = int(input())
         s = input()
         
-        print(sol(s, int(n), c))
+        print(sol1(s ,n))
     
 
     
