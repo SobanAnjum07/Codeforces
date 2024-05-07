8b466faa2570b2ac40455e9fb436d14bfca28dfa
Soban <msobananjum786@gmail.com>
2024-05-07 20:53:48 +0500

    CODE

--- test.py
+++ test.py
@@ -28,21 +28,34 @@ def sol1(s,n):
         ap = s[:i] + s[j+1:]
         
         if hash_map[ap] == 0:
-            hash_map = 1
+            hash_map[ap] = 1
         
         i+=1; j+=1
         
     val = hash_map.values()
     return sum(val)
 
+
+def sol(n, s):
+    sum_  = 0
+    i = 0
+    while i <= n-2:
+        if i == n-2: sum_ += 1 ; break;
+        if s[i] != s[i + 2]:
+            sum_ += 1
+        
+        i += 1
+    return sum_
+            
+
     
 if __name__ == "__main__":
     
     for _ in range(int(input())):
         n = int(input())
-        s = input()
+        s = input().strip()
         
-        print(sol1(s ,n))
+        print(sol(n, s))
     
 
     
