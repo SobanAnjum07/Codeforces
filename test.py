4f080c49f5b99fe7aed7d8e19534b43b7b813e9c
Soban <msobananjum786@gmail.com>
2024-05-07 22:00:07 +0500

    CODE

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-
-
 import sys
 from collections import defaultdict
 input = sys.stdin.readline
@@ -20,35 +18,7 @@ input = sys.stdin.readline
     else:
         return (index_green[-1] + 1) - (index_ele[0]+1)
 """
-def sol1(s,n):
-    i=0; j=1
-    hash_map = defaultdict(int)
-    
-    while j < len(s):
-        ap = s[:i] + s[j+1:]
-        
-        if hash_map[ap] == 0:
-            hash_map[ap] = 1
-        
-        i+=1; j+=1
-        
-    val = hash_map.values()
-    return sum(val)
 
-
-def sol(n, s):
-    sum_  = 0
-    i = 0
-    while i <= n-2:
-        if i == n-2: sum_ += 1 ; break;
-        if s[i] != s[i + 2]:
-            sum_ += 1
-        
-        i += 1
-    return sum_
-            
-
-    
 if __name__ == "__main__":
     
     for _ in range(int(input())):
