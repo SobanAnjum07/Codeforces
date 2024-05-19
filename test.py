0ff0396b121c9d6b11d43b342042a90a9162f7f1
Soban <msobananjum786@gmail.com>
2024-05-20 01:40:38 +0500

    CODE

--- test.py
+++ test.py
@@ -1,28 +1,12 @@
 import sys
 input = sys.stdin.readline
-from collections import Counter
 
-def sol(n: int, s : str):
-    
-    if len(s) == 1: return s
-    
-    hash_map = Counter(s)
-    ret_s = ""
-    
-    for k in hash_map:
-        if hash_map[k] % n != 0:
-            return -1
-
-        ret_s += (k * (hash_map[k] // n ))
-    
-       
-    return ret_s * n    
-    
+def sol():
+    pass
+        
 
 if __name__ == '__main__':
     
     
     n = int(input())
-    s = input().strip()
-    
-    print(sol(n ,s))
+    print(sol(n))
