d2ffa0688a9f0337394654671b03b1aca486cff0
Soban <msobananjum786@gmail.com>
2024-07-08 20:30:19 +0500

    CODE

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 import sys
 input = sys.stdin.readline
-def sol(n):
-    pass
+def sol(a, b):
+    buffer = set(a)
+    counter = len(a)
+    for char in b:
+        if char not in buffer:
+            counter += 1
+
+    return counter
+
 
 if __name__ == "__main__":
-    
-    for _ in range(int(input())):
-        n = int(input())
-        
+
+    for _ in range(int(input().strip())):
+        a = list(input().strip())
+        b = list(input().strip())
+
+        print(sol(a,b))
